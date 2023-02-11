67ed70b52014c0ccf20cd63520325b9a1ad142cb
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-11 12:24:31 +0300

    Saturday 11th

---
+++ arithmetic.py
@@ -0,0 +1,53 @@
+# Addition (+)
+print(2 + 3)
+a = 2
+b = 3
+print('Addition', a + b)
+
+# Subtraction (-)
+print(3 - 2)
+a = 3
+b = 2
+print('Subtraction', a - b)
+
+# Multiplication (*)
+print(2 * 3)
+a = 2
+b = 3
+print('Multiplication', a * b)
+
+# Division (/)
+print(10 / 2)
+a = 10
+b = 2
+print('Division', a / b)
+
+# Area of a triangle
+height = 6
+base = 12
+area = (height * base) / 2
+print(area, 'meters squared')
+
+# 1. Calculate the area of a triangle with a height of 10
+# and a base of 5 (use python variables and print the result)
+# answer = 25
+height = 10
+base = 5
+area = (height * base) / 2
+print(area, 'meters squared')
+
+# 2. Calculate the area of a rectangle with a height of 10 and a width of 5
+# (use python variables and print the result) area = height * width
+# answer = 50
+height = 10
+width = 5
+area = height * width
+print(area, 'meters squared')
+
+# 3. Calculate the perimeter of a rectangle with a height of 10 and
+# a width of 5 (use python variables and print the result)
+# perimeter = 2 * (height + width)
+# answer = 
+height = 10
+width = 5
+perimeter = 2 * (height + width)
