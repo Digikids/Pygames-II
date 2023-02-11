ba500bfa55d6584a7e2669618fdbebdf51f326e3
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-11 12:24:34 +0300

    Saturday 11th

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+# Numbers
+# Integers
+my_age = 10
+print(type(my_age))
+
+# Floats
+my_height = 6.0
+print(type(my_height))
+
+sum = my_age + my_height
+print(type(sum))
+
+# Strings
+my_school = 'Digikids'  # strings are surrounded by single or double quotes
+print(type(my_school))
+my_sentence = "I'm a student at Digikids"
+print(type(my_sentence))
+
+my_name = 'John'
+print('Hello ' + my_name)  # concatenation addition of strings
+
+print(str(my_age) + my_name) # str() converts a number to a string
+print(2 * my_name)  # multiplication of strings
+
+# 1. Write a code to create a new integer variable and 
+# store your favorite number in it.
+
+# 2. Write a code to create a new float variable and 
+# store the value of pi with two decimal places. pi = 3.14
+
+# 3. Write a code to add two floating-point numbers and 
+# store the result in a new variable.
+
+# 4. Write a code to create a new string variable and 
+# store your name.
+
+# 5. Write a code to print a sentence that includes your 
+# name and your favorite number, using the variables you created in 
+# questions 1 and 4.
