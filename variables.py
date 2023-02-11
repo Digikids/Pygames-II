7ad5d60bdc79dcdc08002d57cbf68162dd396e8d
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-11 12:24:41 +0300

    Saturday 11th

---
+++ variables.py
@@ -0,0 +1,27 @@
+my_school = 'Digikids'
+print(my_school)
+
+my_age = 10
+print(my_age)
+
+height = 6
+base = 12
+area = (height * base) / 2
+print(area,'meters squared')
+
+age = 9
+Age = 10
+print(age) # print is a python keyword
+print(Age)
+
+a = 10
+hello = 'Hollo World'
+print(hello)
+
+j = 8
+k = j
+print(k)
+
+my_height = '6'
+int(my_height)
+print(my_height)
