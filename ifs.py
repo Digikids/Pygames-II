27161f7be70738466ba0e81328fa794081f5f55e
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-18 12:24:01 +0300

    Saturday 18th

---
+++ ifs.py
@@ -0,0 +1,22 @@
+today = "Saturday"
+
+if today == "Saturday":
+    print("Party")
+
+myAge = 12
+yourAge = 12
+
+if myAge < yourAge:
+    print("You are older than me")
+elif myAge > yourAge:
+    print("You are younger than me")
+else:
+    print("We are the same age")
+
+
+num = -0.1
+
+if num > 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
