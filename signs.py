f58eeeb2c9d235120a702866934b61eb16c5e22c
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-18 12:24:12 +0300

    Saturday 18th

---
+++ signs.py
@@ -0,0 +1,29 @@
+# Greater than 
+a = 10
+b = 11
+print(a > b)
+
+# Less than
+a = 10
+b = 11
+print(a < b)
+
+# Equal to
+a = 10
+b = 10
+print(a == b)
+
+# Greater than or equal to
+a = 11
+b = 15
+print(a >= b)
+
+# Less than or equal to
+a = 11
+b = 15
+print(a <= b)
+
+# Not equal to
+a = 11
+b = 15
+print(a != b)
