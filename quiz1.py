42fe91d0fa167991df3f560546bd39253eb58e3b
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-18 12:24:09 +0300

    Saturday 18th

---
+++ quiz1.py
@@ -0,0 +1,22 @@
+# 1. Write a program that takes in a user's age
+# and checks if they are old enough to vote (18 years or older).
+print('QUESTION 1')
+print('What is your age?')
+age = input()
+if int(age) >= 18:
+    print('You are old enough to vote!')
+else:
+    print('You are not old enough to vote.')
+
+# 2. Write a program that takes in a number and
+# checks if it is positive or negative.
+print('QUESTION 2')
+print('Enter a number:')
+num = input()
+if int(num) >= 0:
+    print('Your number is positive.')
+else:
+    print('Your number is negative.')
+
+# 3. Write a program that takes in two numbers 
+# and checks if the first number is greater than the second number.
