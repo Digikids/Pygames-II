16f8474ab52358621d35ebfb995e98e21794a497
Stephen Muchendu <muchendumaina66@gmail.com>
2023-03-04 12:10:30 +0300

    Saturday 4 March

---
+++ loops.py
@@ -0,0 +1,32 @@
+# for loops
+# The for loop is used to iterate over a sequence
+# (that is either a list, a tuple, a dictionary, a set, or a string).
+myFriends = ["Jim", "Karen", "Kevin"]
+for item in myFriends:
+    print(item)
+
+myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+             11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+myEvenNumbers = []
+myOddNumbers = []
+for i in myNumbers:
+    if i % 2 == 0:
+        myEvenNumbers.append(i)
+    else:
+        myOddNumbers.append(i)
+print(myEvenNumbers)
+print(myOddNumbers)
+
+myIntegers = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+              10, 11, 12, 13, 14, 15, 16,
+              17, 18, 19, 20, 21, 22, 23,
+              24, 25, 26, 27, 28, 29, 30,
+              31, 32, 33, 34, 35, 36, 37,
+              38, 39, 40, 41, 42, 43, 44,
+              45, 46, 47, 48, 49, 50]
+multiplesOfSeven = []
+for i in myIntegers:
+    if i % 7 == 0:
+        multiplesOfSeven.append(i)
+
+print(multiplesOfSeven)
