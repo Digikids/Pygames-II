ffd34278f134e5f980f8623afa9f9c34e997458d
Stephen Muchendu <muchendumaina66@gmail.com>
2023-03-04 12:04:26 +0300

    Saturday 4 March

---
+++ adventure.py
@@ -0,0 +1,28 @@
+# Text Adventure Game
+
+# define functions for each room
+
+def room1():
+    print("You are in a dark room.")
+    # add options for player to choose from
+    # use input() function to get player's choice
+    # use if/elif statements to determine next step
+
+def room2():
+    print("You are in a bright room.")
+    # add options for player to choose from
+    # use input() function to get player's choice
+    # use if/elif statements to determine next step
+
+# define main function to run the game
+
+def main():
+    print("Welcome to the Text Adventure Game!")
+    # initialize game variables
+    # use while loop to keep the game running
+    # use if/elif statements to determine which room to go to
+    # call room functions based on player's choice
+
+# call main function to start the game
+
+main()
