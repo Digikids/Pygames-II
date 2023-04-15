395e1ab6494686729dc3d10058840675409a9b95
Stephen Muchendu <muchendumaina66@gmail.com>
2023-04-15 20:02:57 +0300

    last

--- while.py
+++ while.py
@@ -1,18 +1,77 @@
-count = 0
+import pygame
+import random
 
-while count < 5:
-    print("The count is", count)
-    count = count + 1
+pygame.init()
 
+# set up the display
+dim = (800, 600)
+screen = pygame.display.set_mode(dim)
+pygame.display.set_caption("Ping Pong")
+
+# Create a function that returns a random color
+
+
+def random_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    return (red, green, blue)
+
+
+# set up the rectangle
+rect_width = 50
+rect_height = 50
+rect_color = random_color()
+rect_x = dim[0] // 2
+rect_y = dim[1] // 2
+rect_x_speed = 0.5
+rect_y_speed = 0.5
+radius = 40
+
+# draw the rectangle on the screen
+
+
+def draw_rect():
+    pygame.draw.rect(screen, rect_color,
+                     (rect_x, rect_y, rect_width, rect_height), radius)
+
+
+# update the screen
+r_c = random_color()
+
+
+def update_screen():
+    screen.fill(r_c)
+    draw_rect()
+    pygame.display.update()
+
+
+update_screen()
+
+# main game loop
 while True:
-    answer = input("Do you want to continue? (y/n): ")
-    if answer.lower() == "n":
-        break
-    print("Continuing...")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
-i = 0
-while i < 10:
-    i = i + 1
-    if i % 2 == 0:
-        continue
-    print(i)
+    # move the rectangle
+    rect_x += rect_x_speed
+    rect_y += rect_y_speed
+
+    if rect_x > dim[0]:
+        rect_x_speed = rect_x_speed * -1
+        rect_color = random_color()
+
+    if rect_x < 0:
+        rect_x_speed = rect_x_speed * -1
+        rect_color = random_color()
+
+    if rect_y > dim[1]:
+        rect_y_speed = rect_y_speed * -1
+        rect_color = random_color()
+
+    if rect_y < 0:
+        rect_y_speed = rect_y_speed * -1
+        rect_color = random_color()
+
+    update_screen()
